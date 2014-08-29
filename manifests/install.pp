38eab58590093064e41868561d7025a211f826e9
Kenny Say <ksay@localnet.com>
2014-08-29 14:39:05 -0400

    Added exec to create file /tmp/aide if it does not exsist.

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ class aide::install inherits aide {
   }
 
   # Create /tmp/aide file if it does not exist - Used with nagios check
-  exec { "/bin/mkdir -p /tmp/aide/":
+  exec { "/usr/bin/touch /tmp/aide":
     unless => "/usr/bin/test -d /tmp/aide"
     }
 }
