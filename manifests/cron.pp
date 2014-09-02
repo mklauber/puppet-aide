afcf3fa66265f66a95d0e016150c2a5b783f94b4
Kenny Say <ksay@localnet.com>
2014-09-02 11:14:18 -0400

    Added cron to output to /var/lib/aide/lastrun instead of sending an email.

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,7 +1,8 @@
 # Class for managing aide's cron job.
 class aide::cron inherits aide {
   cron { 'aide':
-    command => "${aide::command} --check ${aide::check_parameters} | ${aide::mail_path} -s \"\$HOSTNAME - Daily AIDE integrity check\" ${aide::email}",
+    command => "${aide::command} --check ${aide::check_parameters} > /var/lib/aide/lastrun",
+#    command => "${aide::command} --check ${aide::check_parameters} | ${aide::mail_path} -s \"\$HOSTNAME - Daily AIDE integrity check\" ${aide::email}",
 #    command => $::aide::command,
     user    => 'root',
     hour    => $::aide::hour,
