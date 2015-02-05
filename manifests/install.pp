950be5bef61f0d8c83564494d362c8cb18d2fbcf
Kenny Say <ksay@localnet.com>
2015-02-05 13:47:52 -0500

    Added copynewdb to allow for this option in the aide config.

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,5 +8,14 @@ class aide::install inherits aide {
   # Create /var/lib/aide/lastrun file if it does not exist - Used with nagios check
   exec { "/usr/bin/touch /var/lib/aide/lastrun":
     unless => "/usr/bin/test /var/lib/aide/lastrun"
-    }
+  }
+
+  file { '/etc/default/aide':
+    ensure => present,
+  }->
+  file_line { 'Append a line to /etc/default/aide':
+    path   => '/etc/default/aide',  
+    line   => 'COPYNEWDB=no',
+    match  => 'COPYNEWDB=yes',
+  }
 }
