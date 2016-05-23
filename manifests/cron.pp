9dbb053a91cc7e73542cab5bf5fdb0fb588a30fc
Marco Rodrigues <gothicx@gmail.com>
2016-05-23 11:36:29 +0200

    Update cron.pp

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -3,7 +3,8 @@ class aide::cron inherits aide {
   cron { 'aide':
     command => "${::aide::params::aide_path} --check",
     user    => 'root',
-    hour    => $::aide::hour,
+    hour    => '*',
+    minute  => $::aide::minute,
     require => [Package['aide'], Exec['install aide db']]
   }
 }
